f7ae0a2082855dac57e05e0b899c68b02f570306
Tucker Hermans <thermans@cc.gatech.edu>
2010-03-05 10:37:44 -0500

    Added constants to the grip control node.

--- pioneer_control/src/pioneer_control_node.py
+++ pioneer_control/src/pioneer_control_node.py
@@ -3,17 +3,35 @@ import roslib; roslib.load_manifest('pioneer_control')
 import rospy
 from p2os.msg import GripperState
 
+# Gripper commands
+GRIP_OPEN   = 1
+GRIP_CLOSE  = 2
+GRIP_STOP   = 3
+LIFT_UP     = 4
+LIFT_DOWN   = 5
+LIFT_STOP   = 6
+GRIP_STORE  = 7
+GRIP_DEPLOY = 8
+GRIP_HALT   = 15
+GRIP_PRESS  = 16
+LIFT_CARRY  = 17
+
 def gripControl():
     pub = rospy.Publisher("grip_command", GripperState)
     rospy.init_node("grip_control")
 
-    while not rospy.is_shutdown():
-        grip_cmd = GripperState()
-        grip_cmd.grip.state = 2 # Tell the gripper to open
-        pub.publish(grip_cmd)
-        rospy.sleep(1.0)
+    # Run a single command to close and lift the gripper
+    grip_cmd = GripperState()
+    grip_cmd.grip.state = GRIP_CLOSE
+    grip_cmd.lift.state = LIFT_UP
+    pub.publish(grip_cmd)
 
 if __name__ == '__main__':
     try:
         gripControl()
     except rospy.ROSInterruptException: pass
+
+
+
+
+
