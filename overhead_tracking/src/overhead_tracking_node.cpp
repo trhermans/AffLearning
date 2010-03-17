fb231455a3ce95f856859cbce75f4f33d3b05e52
Tucker Hermans <thermans@cc.gatech.edu>
2010-03-17 15:33:02 -0400

    Publish robot pose and cleanup object poses.

--- overhead_tracking/src/overhead_tracking_node.cpp
+++ overhead_tracking/src/overhead_tracking_node.cpp
@@ -38,6 +38,9 @@
 
 #include "overhead_tracking.h"
 #include "bg_subtract.h"
+#include "overhead_tracking/CleanupObjectArray.h"
+#include "overhead_tracking/CleanupObject.h"
+#include "geometry_msgs/Pose2D.h"
 
 class OverheadTrackingNode
 {
@@ -48,6 +51,9 @@ class OverheadTrackingNode
   {
     image_sub_ = it_.subscribe("image_topic", 1,
                                &OverheadTrackingNode::imageCallback, this);
+    pose_pub = n_.advertise<geometry_msgs::Pose2D>("robot_pose", 1000);
+    object_pub = n_.advertise<overhead_tracking::CleanupObjectArray>(
+        "cleanup_objects", 1000);
   }
 
   // Publish and Subscribe methods
@@ -81,12 +87,37 @@ class OverheadTrackingNode
     tracker_.updateDisplay(current_img_, contours_);
   }
 
+  void publishData()
+  {
+    pose_pub.publish(tracker_.getRobotPose());
+
+    // Convert visual objects to cleanup objects
+    std::vector<OverheadVisualObject> overhead_objs =
+        tracker_.getTrackedObjects();
+    overhead_tracking::CleanupObjectArray cleanup_objs;
+    for(unsigned int i = 0; i < overhead_objs.size(); ++i)
+    {
+      cleanup_objs.objects.push_back(overhead_objs[i].state);
+    }
+    object_pub.publish(cleanup_objs);
+  }
+
+  void spin()
+  {
+    while(n_.ok())
+    {
+      publishData();
+      ros::spinOnce();
+    }
+  }
+
  protected:
   ros::NodeHandle n_;
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   sensor_msgs::CvBridge bridge_;
 
+  ros::Publisher pose_pub, object_pub;
   OverheadTracker tracker_;
   BgSubtractGUI bg_gui_;
   cv::Mat current_img_;
@@ -98,6 +129,6 @@ int main(int argc, char ** argv)
   ros::init(argc, argv, "overhead_tracking");
   ros::NodeHandle n;
   OverheadTrackingNode overhead_node(n);
-  ros::spin();
+  overhead_node.spin();
 }
 
